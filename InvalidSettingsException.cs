e7e55443fe17c7f2512374dc0e6271c904276889
iamlegendxi <legend66566@gmail.com>
2020-12-19 16:42:22 -0700

    Added base settings for scrabble board

---
+++ InvalidSettingsException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrabbleMaker
+{
+    class InvalidSettingsException : Exception
+    {
+
+        public InvalidSettingsException()
+        {
+        }
+
+        public InvalidSettingsException(string message) : base(message)
+        {
+        }
+
+    }
+}
--- ScrabbleBoard.cs
+++ ScrabbleBoard.cs
@@ -11,16 +11,52 @@ namespace ScrabbleMaker
         private Cell[,] Board;
         
         //Default ScrabbleBoard constructor
-        public ScrabbleBoard()
+        public ScrabbleBoard() : this(15)
         {
-            Board = new Cell[15, 15];
+            //InsertDefaultTWS();
+            //InsertDefaultDWS();
+            //InsertDefaultTLS();
+            //InsertDefaultDLS();
         }
 
+        //Scrabble board constructor with defined side sizes
+        public ScrabbleBoard(int size)
+        {
+            if (size % 3 != 0) throw new InvalidSettingsException("Scrabble board size must be divisible by 3 when using default modifier generation.");
+            Board = new Cell[size-1, size-1];
+        }
+
+        /* TODO:
+         * Resize game board with scrabble board sizes (size cap/scroll bar?)
+         * Add default modifier generation
+         */
+
+        //Cells represent individual tiles on the board
         protected class Cell
         {
-            public Cell()
+            public enum Modifier
             {
+                WORD, //Tile provides a word multiplier
+                LETTER, //Tile provides a letter multiplier
+                NONE //Tile provides no bonusees
+            }
 
+            //Modifier bonus provided when a word is played on a tile
+            public Modifier ModifierType { get; private set; }
+
+            //Numerical score multiplier when modifier conditions are met. If the NONE modifier type is selected, this will always be 1
+            public int ModifierAmount { get; private set; }
+
+            //Default cell constructor
+            public Cell() : this(Modifier.NONE, 1)
+            {
+            }
+
+            //Create cells with modifiers to point values
+            public Cell(Modifier modifier, int amt)
+            {
+                ModifierType = modifier;
+                ModifierAmount = amt;
             }
         }
     }
